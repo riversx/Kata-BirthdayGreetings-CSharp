74d40ed2ec60cdc518f72d26314b19a1f78faa96
Boris Sclauzero <boris.sclauzero@zupit.it>
2020-02-14 18:29:49 +0100

    update to dotnet core 3.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Birthday Greetings Kata in C# 
+# Birthday Greetings Kata in C
 
 This is a porting to C# of the BirthdayGreetings Kata of Matteo Vaccari.
 The original cata [can be found here](https://github.com/xpmatteo/birthday-greetings-kata).
@@ -7,4 +7,16 @@ The exercise that is meant to teach something about dependency inversion and dep
 
 This is a refactoring of the BirthdayGreetings Kata C# of [mamoo/birthday-greetings-kata-csharp](https://github.com/mamoo/birthday-greetings-kata-csharp).
 
-*This refactoring has been made to practice the kata on Mac OSX using VistualStudio for Mac*
+_This refactoring has been made to practice the kata on Mac OSX using VisuaStudio-Code -or- VistualStudio for Mac_
+
+_14/02/2020 Updated to DotNetCore 3.1_
+
+## To run the tests
+
+```
+dotnet test
+```
+
+## Warning
+
+A little bug has been intentionally introduced in the source code to check the test runtime.
---
+++ test/BirthdayGreetingsKata.Test/AcceptanceTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using netDumbster.smtp;
+using NUnit.Framework;
+
+namespace BirthdayGreetings.Tests
+{
+    public class AcceptanceTest
+    {
+        const int NONSTANDARD_PORT = 3003;
+        SimpleSmtpServer server;
+        SmtpMessage[] emailIterator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            server = SimpleSmtpServer.Start(NONSTANDARD_PORT);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            server.Stop();
+        }
+
+        [Test]
+        public void SendGreetings()
+        {
+            ExecuteBirthdayServiceFor(@"resources/employee_data.txt", "2008/10/08");
+            AssertExpectedNumberOfEmailSentIs(1);
+            AssertExpectedEmailWith_Subject_andBody_andSentTo("Happy Birthday!", "Happy Birthday, dear John!", "john.doe@foobar.com");
+        }
+
+        void AssertExpectedEmailWith_Subject_andBody_andSentTo(string subject, string body, string recipient)
+        {
+            var message = emailIterator.First();
+            Assert.AreEqual(body, message.MessageParts[0].BodyData);
+            Assert.AreEqual(subject, message.Headers["Subject"]);
+            Assert.AreEqual(recipient, message.Headers["To"]);
+        }
+
+        void AssertExpectedNumberOfEmailSentIs(int expected)
+        {
+            Assert.AreEqual(expected, server.ReceivedEmail.Length);
+        }
+
+        void ExecuteBirthdayServiceFor(string employeeFileName, string date)
+        {
+            var service = new BirthdayService();
+            service.SendGreetings(employeeFileName, new XDate(date), "localhost", NONSTANDARD_PORT);
+            emailIterator = server.ReceivedEmail;
+        }
+    }
+}
