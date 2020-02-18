4b2646037ed4361edc88afcaccaa36982b5f3beb
Boris Sclauzero <boris.sclauzero@zupit.it>
2020-02-18 21:46:48 +0100

    adds other thest with mocks on BirthdayServiceTest

--- test/BirthdayGreetingsKata.Test/BirthdayServiceTests.cs
+++ test/BirthdayGreetingsKata.Test/BirthdayServiceTests.cs
@@ -23,5 +23,43 @@ namespace BirthdayGreetings.Tests
             // assert
             messageService.DidNotReceiveWithAnyArgs().SendGreetingsToEmplyee(default);
         }
+
+        [Test]
+        public void SendGreetings_WithTwoElementsRepository_SendsOneMessages()
+        {
+            // arrange
+            IEmployeesRepository employeesRepository = Substitute.For<IEmployeesRepository>();
+            employeesRepository.GetAll().Returns(new List<Employee>{
+                new Employee { FirstName = "Mario", LastName = "Rossi", BirthDate = new XDate("2020/02/18") },
+                new Employee { FirstName = "Pippo", LastName = "Baudo", BirthDate =new XDate("2020/02/19")  }
+            });
+            IMessageService messageService = Substitute.For<IMessageService>();
+            var birthdayService = new BirthdayService(employeesRepository, messageService);
+
+            // act
+            birthdayService.SendGreetings(new XDate("2020/02/18"));
+
+            // assert
+            messageService.ReceivedWithAnyArgs(1).SendGreetingsToEmplyee(default);
+        }
+
+        [Test]
+        public void SendGreetings_WithTwoElementsRepository_SendsTwoMessages()
+        {
+            // arrange
+            IEmployeesRepository employeesRepository = Substitute.For<IEmployeesRepository>();
+            employeesRepository.GetAll().Returns(new List<Employee>{
+                new Employee { FirstName = "Mario", LastName = "Rossi", BirthDate = new XDate("2020/02/18") },
+                new Employee { FirstName = "Pippo", LastName = "Baudo", BirthDate =new XDate("2020/02/18")  }
+            });
+            IMessageService messageService = Substitute.For<IMessageService>();
+            var birthdayService = new BirthdayService(employeesRepository, messageService);
+
+            // act
+            birthdayService.SendGreetings(new XDate("2020/02/18"));
+
+            // assert
+            messageService.ReceivedWithAnyArgs(2).SendGreetingsToEmplyee(default);
+        }
     }
 }
