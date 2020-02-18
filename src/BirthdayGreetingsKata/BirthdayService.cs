43b3ed32399a1f3704bcef3ec8c769359a8da905
Boris Sclauzero <boris.sclauzero@zupit.it>
2020-02-18 18:15:00 +0100

    estratto servizio SmtpMessageService

--- src/BirthdayGreetingsKata/BirthdayService.cs
+++ src/BirthdayGreetingsKata/BirthdayService.cs
@@ -1,8 +1,5 @@
 ﻿// using System.Net.Mail;
 using System.Collections.Generic;
-using System.IO;
-using MailKit.Net.Smtp;
-using MimeKit;
 
 namespace BirthdayGreetings
 {
@@ -15,48 +12,17 @@ namespace BirthdayGreetings
                 var repositoy = new FileEmployeesRepository(fileName);
                 List<Employee> employees = repositoy.GetAll();
 
+                var messageService = new SmtpMessageService();
                 foreach (var employee in employees)
                 {
                     if (employee.IsBirthday(xDate))
                     {
-                        var recipient = employee.Email;
-                        var body = string.Format("Happy Birthday, dear {0}!", employee.FirstName);
-                        var subject = "Happy Birthday!";
-                        SendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);
+                        messageService.SendGreetingsToEmplyee(smtpHost, smtpPort, employee);
                     }
                 }
             }
 
         }
 
-        void SendMessage(string smtpHost, int smtpPort, string sender, string subject, string body, string recipient)
-        {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(sender));
-            message.To.Add(new MailboxAddress(recipient));
-            message.Subject = subject;
-            message.Body = new TextPart("plain")
-            {
-                Text = body
-            };
-            // Send the message
-            SendMessage(smtpHost, smtpPort, message);
-        }
-
-        // made protected for testing :-( 
-        // this was originally needed in java  - check if is needed here as well.
-        protected void SendMessage(string smtpHost, int smtpPort, MimeMessage message)
-        {
-            using (var smtpClient = new SmtpClient())
-            {
-                smtpClient.Connect(smtpHost, smtpPort, false);
-                // smtpClient.AuthenticationMechanisms.Remove("XOAUTH2");
-                // // Note: since we don't have an OAuth2 token, disable 	
-                // // the XOAUTH2 authentication mechanism.     
-                // smtpClient.Authenticate("info@MyWebsiteDomainName.com", "myIDPassword");
-                smtpClient.Send(message);
-                smtpClient.Disconnect(true);
-            }
-        }
     }
 }
---
+++ src/BirthdayGreetingsKata/SmtpMessageService.cs
@@ -0,0 +1,51 @@
+// using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace BirthdayGreetings
+{
+    internal class SmtpMessageService
+    {
+        public SmtpMessageService()
+        {
+        }
+
+        internal void SendGreetingsToEmplyee(string smtpHost, int smtpPort, Employee employee)
+        {
+            var recipient = employee.Email;
+            var body = string.Format("Happy Birthday, dear {0}!", employee.FirstName);
+            var subject = "Happy Birthday!";
+            SendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient);
+        }
+
+        void SendMessage(string smtpHost, int smtpPort, string sender, string subject, string body, string recipient)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(sender));
+            message.To.Add(new MailboxAddress(recipient));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+            // Send the message
+            SendMessage(smtpHost, smtpPort, message);
+        }
+
+        // made protected for testing :-( 
+        // this was originally needed in java  - check if is needed here as well.
+        protected void SendMessage(string smtpHost, int smtpPort, MimeMessage message)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect(smtpHost, smtpPort, false);
+                // smtpClient.AuthenticationMechanisms.Remove("XOAUTH2");
+                // // Note: since we don't have an OAuth2 token, disable 	
+                // // the XOAUTH2 authentication mechanism.     
+                // smtpClient.Authenticate("info@MyWebsiteDomainName.com", "myIDPassword");
+                smtpClient.Send(message);
+                smtpClient.Disconnect(true);
+            }
+        }
+    }
+}
