c98515bcdc00faa43c42f2634e0df4da15302321
Boris Sclauzero <boris.sclauzero@zupit.it>
2020-02-18 21:40:26 +0100

    adds test with Mocks

---
+++ test/BirthdayGreetingsKata.Test/BirthdayServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using NSubstitute;
+using System.Collections.Generic;
+
+namespace BirthdayGreetings.Tests
+{
+    public class BirthdayServiceTests
+    {
+
+        [Test]
+        public void SendGreetings_WithEmptyRepository_SendsZeroMessages()
+        {
+            // arrange
+            IEmployeesRepository employeesRepository = Substitute.For<IEmployeesRepository>();
+            employeesRepository.GetAll().Returns(new List<Employee>());
+            IMessageService messageService = Substitute.For<IMessageService>();
+            var birthdayService = new BirthdayService(employeesRepository, messageService);
+
+            // act
+            birthdayService.SendGreetings(new XDate("2020/02/18"));
+
+            // assert
+            messageService.DidNotReceiveWithAnyArgs().SendGreetingsToEmplyee(default);
+        }
+    }
+}
